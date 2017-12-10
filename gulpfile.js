aa7f8337d084704c87d50c00f27783b06165fcc4
BrainWosher <svprokopenko92@gmail.com>
2017-12-10 13:23:37 +0700

    delete old build. Install sass browserSync and bower

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,27 @@
 'use strict';
 
 var gulp = require('gulp');
-    pug = require('gulp-pug');
+var	sass = require('gulp-sass');
+var browserSync = require('browser-sync');
     
     
-gulp.task('pug', function() {
-    return gulp.src('src/pug/pages/*.pug')
-    .pipe(pug({
-        pretty:true
-    }))
-    .pipe(gulp.dest('build'));
-})
+    
+ggulp.task('sass', function(){ // Создаем таск Sass
+    return gulp.src('app/sass/**/*.sass') // Берем источник
+        .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+        .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
+        .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
+});
+gulp.task('browser-sync', function() { // Создаем таск browser-sync
+    browserSync({ // Выполняем browser Sync
+        server: { // Определяем параметры сервера
+            baseDir: 'app' // Директория для сервера - app
+        },
+        notify: false // Отключаем уведомления
+    });
+});
+gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'], function() {
+	gulp.watch('app/sass/**/*.sass', ['sass']); // Наблюдение за sass файлами в папке sass
+	gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
+	gulp.watch('app/js/**/*.js', browserSync.reload);   // Наблюдение за JS файлами в папке js
+});
